0da1379013416beb816cff6bde6e173519acf91d
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-15 12:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  // Trim the string to remove leading and trailing spaces
+  const trimmedStr = str.trim();
+
+  // Check if the trimmed string is not empty
+  if (trimmedStr !== '') {
+    // Return the first character of the trimmed string
+    return trimmedStr[0];
+  } else {
+    // Return an empty string if the input is empty or contains only spaces
+    return '';
+  }
 }
 
 // Do not change the code below
